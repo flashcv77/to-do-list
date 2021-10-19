38c7d9729cbbdd0cca6f60767cd78d40d00d4ea7
Andriy Martynyuk <flashcv77@gmail.com>
2021-10-19 11:31:57 +0300

    validated-form almost done

--- src/components/ValidatedForm/ValidatedForm.js
+++ src/components/ValidatedForm/ValidatedForm.js
@@ -1,85 +1,74 @@
 import React from "react";
 import Button from "../Button";
-import "./ValidatedForm.css"
+import "./ValidatedForm.scss"
 
 
 class ValidatedForm extends React.Component {
     state = {
         fields: {
             inputName: {
-                name: "inputName", //fields.inputName , <label for="name"></label>
-                label: "Name", //<label> ... </label>
-                value: "", // |_value_______| <- input
-                error: '',
+                name: "inputName",
+                label: "Name",
+                value: "",
+                error: "",
                 validator: (value = "") => {
-                    return value.length > 7 ? false : "Name should be at least 6 symbols";
+                    return value.length >= 4 ? false : "Name should be at least 4 symbols";
                 },
             },
             email: {
                 name: "email",
-                label: "email",
+                label: "Email",
                 value: "",
-                error: null,
+                error: "",
                 validator: (value = "") => {
-                    return value ? false : "Required";
+                    return value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g) ? false : "Email is not valid";
                 },
             },
             password: {
                 name: "password",
-                label: "password",
+                label: "Password",
                 value: "",
-                error: null,
+                error: "",
                 validator: (value = "") => {
-                    return value ? false : "Required";
+                    return value.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/g) ? false : "Password is not valid, minimum 8 symbols, at least one letter and one number  ";
                 },
             },
             confirmPassword: {
                 name: "confirmPassword",
-                label: "confirm password",
+                label: "Confirm password",
                 value: "",
-                error: null,
+                error: "",
                 validator: (value = "") => {
-                    return value ? false : "Required";
+                    return value === this.state.fields.password.value ? false : "Passwords dose not match";
                 },
             },
         }
     }
 
     handleChange = (event) => {
-        const { value, name } = event.target;   
+        const { value, name } = event.target;
         const currentField = this.state.fields[name];
-        const error = currentField.validator(value);
+        let error = currentField.validator(value);
         this.setState({
             fields: {
                 ...this.state.fields,
                 [name]: { ...currentField, value, error },
             },
         });
+    }
 
-    };
-
-    handleReset = () => {
-        console.log('check');
+    handleReset = (event) => {
+        // event.preventDefault();
+        const { fields } = this.state;
+        for (let key in fields) {
+            fields[key].error = '';
+            fields[key].value = '';
+        }
         this.setState({
             fields: {
-                ...this.state.fields,
-                inputName: {
-                    ...this.state.fields.inputName, value: '',
-                },
-                email: {
-                    ...this.state.fields.email, value: '',
-                },
-                password: {
-                    ...this.state.fields.password, value: '',
-                },
-                confirmPassword: {
-                    ...this.state.fields.confirmPassword, value: '',
-                },
-
+                ...this.state.fields
             }
-
         });
-        console.log(this.state);
     }
 
 
@@ -90,57 +79,89 @@ class ValidatedForm extends React.Component {
         console.log("email: " + fields.email.value)
         console.log("password: " + fields.password.value)
         console.log("confirmed password: " + fields.confirmPassword.value)
-        // let value1 = this.state.fields.inputName.value;
-        // console.log(value1);
-        // console.log(name, label, value);
-        //     this.setState({
-        //         value: value,
-        //     });
         this.handleReset();
-    };
+    }
+
+    handleValidate = (event) => {
+        event.preventDefault();
+        console.log("Fields didn't pass validation");
+    }
 
     render() {
         const { fields } = this.state;
-        let arr = Object.entries(this.state);
-        console.log(arr);
+        const errors = [];
+        for (let key in fields) {
+            errors.push(fields[key].error);
+        }
+        console.log(errors);
         return (
             <>
-                <form onSubmit={this.handleSubmit}>
-                    <label htmlFor={fields.inputName.name}>{fields.inputName.label}</label>
-                    <input
-                        value={fields.inputName.value}
-                        onChange={this.handleChange}
-                        name={fields.inputName.name}
-                        id={fields.inputName.name}
-                    />
-                    <div className="error"> {fields.inputName.error && <span>{fields.inputName.error}</span>}</div>
-                    <label htmlFor={fields.email.name}>{fields.email.label}</label>
-                    <input
-                        value={fields.email.value}
-                        onChange={this.handleChange}
-                        name={fields.email.name}
-                        id={fields.email.name}
-                    />
-                    <label htmlFor={fields.password.name}>{fields.password.label}</label>
-                    <input
-                        type="password"
-                        value={fields.password.value}
-                        onChange={this.handleChange}
-                        name={fields.password.name}
-                        id={fields.password.name}
-                    />
-                    <label htmlFor={fields.confirmPassword.name}>{fields.confirmPassword.label}</label>
-                    <input
-                        type="password"
-                        value={fields.confirmPassword.value}
-                        onChange={this.handleChange}
-                        name={fields.confirmPassword.name}
-                        id={fields.confirmPassword.name}
-                    />
-                    <Button buttonName={"Submit"} onClick={this.handleSubmit} />
+                <form onSubmit={errors.some(elem => elem !== false) ? this.handleValidate : this.handleSubmit}>
+                    <h1>Form</h1>
+                    <div className="input-container">
+                        <label htmlFor={fields.inputName.name}>{fields.inputName.label}</label>
+                        <input
+                            autoComplete="off"
+                            className={fields.inputName.error ? "invalid" : "valid"}
+                            placeholder={fields.inputName.label + "..."}
+                            value={fields.inputName.value}
+                            onChange={this.handleChange}
+                            name={fields.inputName.name}
+                            id={fields.inputName.name}
+                        />
+                        <div className="error"> {fields.inputName.error && <span>{fields.inputName.error}</span>}</div>
+                    </div>
+
+                    <div className="input-container">
+                        <label htmlFor={fields.email.name}>{fields.email.label}</label>
+                        <input
+                            autoComplete="off"
+                            className={fields.email.error ? "invalid" : "valid"}
+                            placeholder={fields.email.label + "..."}
+                            value={fields.email.value}
+                            onChange={this.handleChange}
+                            name={fields.email.name}
+                            id={fields.email.name}
+                        />
+                        <div className="error"> {fields.email.error && <span>{fields.email.error}</span>}</div>
+                    </div>
+
+                    <div className="input-container">
+                        <label htmlFor={fields.password.name}>{fields.password.label}</label>
+                        <input
+                            autoComplete="off"
+                            className={fields.password.error ? "invalid" : "valid"}
+                            placeholder={fields.password.label + "..."}
+                            type="password"
+                            value={fields.password.value}
+                            onChange={this.handleChange}
+                            name={fields.password.name}
+                            id={fields.password.name}
+                        />
+                        <div className="error"> {fields.password.error && <span>{fields.password.error}</span>}</div>
+                    </div>
+
+                    <div className="input-container">
+                        <label htmlFor={fields.confirmPassword.name}>{fields.confirmPassword.label}</label>
+                        <input
+                            autoComplete="off"
+                            className={fields.confirmPassword.error ? "invalid" : "valid"}
+                            placeholder={fields.confirmPassword.label + "..."}
+                            type="password"
+                            value={fields.confirmPassword.value}
+                            onChange={this.handleChange}
+                            name={fields.confirmPassword.name}
+                            id={fields.confirmPassword.name}
+                        />
+                        <div className="error"> {fields.confirmPassword.error && <span>{fields.confirmPassword.error}</span>}</div>
+                    </div>
+
+                    <Button buttonName={"Submit"} onClick={errors.some(elem => elem !== false) ? this.handleValidate : this.handleSubmit} />
+
                 </form>
-                {/* <Button buttonName={"Reset"} onClick={this.handleReset} /> */}
                 <button onClick={this.handleReset}>Reset</button>
+                {/* <Button buttonName={"Reset"} onClick={this.handleReset} /> */}
+
             </>
         )
     }
