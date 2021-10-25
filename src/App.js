de7e43fcfa8560143e901f2cb0e76edfba05915d
Andriy Martynyuk <flashcv77@gmail.com>
2021-10-25 09:55:00 +0300

    router is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.scss';
 import ToDoList from './components/ToDoList';
-import FirstComponent from './components/FirstComponent';
 import ValidatedForm from './components/ValidatedForm';
 import FinalForm from './components/FinalForm';
 import PageNotFound from './components/PageNotFound';
 
-import { BrowserRouter as Router, Switch, Route, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 
 function App() {
 
@@ -13,7 +12,8 @@ function App() {
     <Router>
       <div className="App">
         <aside>
-          <ul class="nav-bar">
+          <ul className="nav-bar">
+            <li><NavLink activeClassName="nav-active" to="/"></NavLink></li>
             <li><NavLink activeClassName="nav-active" to="/ToDoList">ToDo List</NavLink></li>
             <li><NavLink activeClassName="nav-active" to="/validated-form">Validated Form</NavLink></li>
             <li><NavLink activeClassName="nav-active" to="/final-form">Final Form</NavLink></li>
@@ -21,16 +21,12 @@ function App() {
         </aside>
         <header className="App-header">
           <Switch>
+            <Route exact path="/" />
             <Route exact path="/ToDoList" component={ToDoList} />
             <Route exact path="/validated-form" component={ValidatedForm} />
             <Route exact path="/final-form" component={FinalForm} />
             <Route component={PageNotFound} />
           </Switch>
-          {/* TO DO LIST */}
-          {/* <ToDoList /> */}
-          {/* <div className="wrapper"><FirstComponent CHECK/></div> */}
-          {/* <ValidatedForm /> */}
-          {/* <FinalForm /> */}
         </header>
 
       </div>
